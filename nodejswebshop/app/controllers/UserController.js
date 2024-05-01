4ec3d765f780a6b4f432279ce1bba6a93ed577e4
Amir Zeqiri <amir.zeqiri@eduvaud.ch>
2024-05-01 11:24:47 +0200

    fin journée 01.05.24

--- nodejswebshop/app/controllers/UserController.js
+++ nodejswebshop/app/controllers/UserController.js
@@ -3,3 +3,23 @@ module.exports = {
         res.send("User: AmirZeqiri");
     }
 };
+
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent default form submission
+
+    // Get input values
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+
+    // You can add your validation logic here
+
+    // For demonstration purposes, just displaying the input values
+    console.log('Username:', username);
+    console.log('Password:', password);
+
+    // You can perform further actions like sending the data to a server for validation
+    // For this example, let's just reset the form
+    this.reset();
+});
+
+
